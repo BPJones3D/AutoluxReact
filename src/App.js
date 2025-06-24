945cfae7ff19a5ab684b6410d8da711770574f69
BPJones3D <bpjones3d@gmail.com>
2025-06-24 14:00:40 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Banner from './components/Banner'
+import NavMenu from './components/NavMenu';
+
+function App() {
+  return (
+    <>
+      <Banner/>
+      <NavMenu/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import './Banner-module.css'
+
+function Banner()
+{
+    return (
+        <header>
+            <h1 className="text-center bg-dark text-white p-3 m-0 fs-2 siteTitle">AUTOLUX CAR DEALERSHIP</h1>
+        </header>
+    )
+}
+
+export default Banner;
